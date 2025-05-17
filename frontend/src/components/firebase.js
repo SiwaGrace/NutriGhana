205af465f99583d1fc08ed524b94424b6bc1a6ac
philip <philipnicholas386@gmail.com>
2025-05-17 17:24:43 +0000

    Add Firebase config and initial setup

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -1,14 +1,13 @@
-// src/component/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCi3JarsLMN3CQ6Lh54Pjc6S7xtE0eXlh8",
-  authDomain: "login1-c931f.firebaseapp.com",
-  projectId: "login1-c931f",
-  storageBucket: "login1-c931f.firebasestorage.app",
-  messagingSenderId: "933527804042",
-  appId: "1:933527804042:web:b439c98d16396ebf013573",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
