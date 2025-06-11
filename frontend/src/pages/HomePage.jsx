89dca907d52f057c3a127bc396349aaa8912daf6
missesenam <moneyesenam@gmail.com>
2025-06-11 10:00:05 +0100

    my commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -35,34 +35,36 @@ const ProfileHome = () => {
           </button>
         ))}
       </div>
+      {/* depends on selected day */}
+      <div>
+        {/* Calories Card */}
+        <div className="mt-4 bg-white p-4 rounded-lg shadow-md flex justify-between items-center">
+          <div>
+            <h3 className="text-2xl font-bold">9989</h3>
+            <p className="text-gray-500 text-sm">Calories left</p>
+          </div>
+          <div className="w-16 h-16 flex items-center justify-center rounded-full border-4 border-gray-200">
+            <img src={StreakIcon} alt="Fire" className="w-6 h-6" />
+          </div>
+        </div>
 
-      {/* Calories Card */}
-      <div className="mt-4 bg-white p-4 rounded-lg shadow-md flex justify-between items-center">
-        <div>
-          <h3 className="text-2xl font-bold">9989</h3>
-          <p className="text-gray-500 text-sm">Calories left</p>
-        </div>
-        <div className="w-16 h-16 flex items-center justify-center rounded-full border-4 border-gray-200">
-          <img src={StreakIcon} alt="Fire" className="w-6 h-6" />
-        </div>
-      </div>
-
-      {/* Macros */}
-      <div className="mt-4 flex gap-3">
-        <div className="flex-1 bg-white p-4 rounded-lg shadow-md text-center">
-          <img src={FishIcon} alt="Protein" className="w-5 h-5 mx-auto" />
-          <p className="text-xs text-gray-500">Protein left</p>
-          <p className="h-1 mt-2 bg-gray-200 w-full"></p>
-        </div>
-        <div className="flex-1 bg-white p-4 rounded-lg shadow-md text-center">
-          <img src={CarbIcon} alt="Carbs" className="w-5 h-5 mx-auto" />
-          <p className="text-xs text-gray-500">Carbs left</p>
-          <p className="h-1 mt-2 bg-gray-200 w-full"></p>
-        </div>
-        <div className="flex-1 bg-white p-4 rounded-lg shadow-md text-center">
-          <img src={FatIcon} alt="Fat" className="w-5 h-5 mx-auto" />
-          <p className="text-xs text-gray-500">Fat left</p>
-          <p className="h-1 mt-2 bg-gray-200 w-full "></p>
+        {/* Macros */}
+        <div className="mt-4 flex gap-3">
+          <div className="flex-1 bg-white p-4 rounded-lg shadow-md text-center">
+            <img src={FishIcon} alt="Protein" className="w-5 h-5 mx-auto" />
+            <p className="text-xs text-gray-500">Protein left</p>
+            <p className="h-1 mt-2 bg-gray-200 w-full"></p>
+          </div>
+          <div className="flex-1 bg-white p-4 rounded-lg shadow-md text-center">
+            <img src={CarbIcon} alt="Carbs" className="w-5 h-5 mx-auto" />
+            <p className="text-xs text-gray-500">Carbs left</p>
+            <p className="h-1 mt-2 bg-gray-200 w-full"></p>
+          </div>
+          <div className="flex-1 bg-white p-4 rounded-lg shadow-md text-center">
+            <img src={FatIcon} alt="Fat" className="w-5 h-5 mx-auto" />
+            <p className="text-xs text-gray-500">Fat left</p>
+            <p className="h-1 mt-2 bg-gray-200 w-full "></p>
+          </div>
         </div>
       </div>
 
