73c6ffcedda008cbe95bf07f3bcb3bd4880e4e8e
missesenam <moneyesenam@gmail.com>
2025-08-14 16:47:09 +0100

    my commit

--- Backend/index.js
+++ Backend/index.js
@@ -43,6 +43,6 @@ mongoose
     process.exit(1);
   });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
