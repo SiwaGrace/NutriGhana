7220fcfc290b2cb587bd72c6be867365fcb4f8cb
missesenam <moneyesenam@gmail.com>
2025-10-08 16:48:59 +0000

    added origin

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    ,
+    "https://incandescent-gnome-cedb70.netlify.app/",
+  ],
   // , "https://your-frontend.com"
   methods: ["GET", "POST", "DELETE", "PUT"],
   credentials: true,
